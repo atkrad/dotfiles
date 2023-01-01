859725bcdb7f74401ec5a0d745faf1a55613034f
Mohammad Abdolirad <m.abdolirad@gmail.com>
2023-01-01 11:50:20 +0100

    Update documentation

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -13,12 +13,11 @@
     # inputs.hardware.nixosModules.common-cpu-amd
     # inputs.hardware.nixosModules.common-ssd
 
-    # You can also split up your configuration and import pieces of it here:
-    # ./users.nix
+    # I also split up my configuration and import pieces of it here:
+    ./locate
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
-    ./locate
   ];
 
   nixpkgs = {
