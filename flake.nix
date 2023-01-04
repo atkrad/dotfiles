c3e3c892aa58b9298b0b5a9a2ea9efda404489e2
Mohammad Abdolirad <m.abdolirad@gmail.com>
2023-01-04 17:03:10 +0100

    Follow exist inputs

--- flake.nix
+++ flake.nix
@@ -5,14 +5,32 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
+    # Pure Nix flake utility functions
+    flake-utils.url = "github:numtide/flake-utils";
+
     # Nix-LD, Run unpatched dynamic binaries on NixOS.
-    nix-ld.url = "github:Mic92/nix-ld";
-    nix-alien.url = "github:thiagokokada/nix-alien";
+    nix-ld = {
+      url = "github:Mic92/nix-ld";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        utils.follows = "flake-utils";
+      };
+    };
+    nix-alien = {
+      url = "github:thiagokokada/nix-alien";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
 
     # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        utils.follows = "flake-utils";
+      };
     };
 
     # Delta is a syntax-highlighting pager for git, diff, and grep output.
@@ -31,25 +49,18 @@
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       inherit (self) outputs;
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
     in
     rec {
       # Your custom packages
       # Acessible through 'nix build', 'nix shell', etc
-      packages = forAllSystems (system:
+      packages = inputs.flake-utils.lib.eachDefaultSystem (system:
         let pkgs = nixpkgs.legacyPackages.${system};
         in import ./pkgs { inherit pkgs; }
       );
 
       # Devshell for bootstrapping
       # Acessible through 'nix develop' or 'nix-shell' (legacy)
-      devShells = forAllSystems (system:
+      devShells = inputs.flake-utils.lib.eachDefaultSystem (system:
         let pkgs = nixpkgs.legacyPackages.${system};
         in import ./shell.nix { inherit pkgs; }
       );
