1ea9fa8b5846b20047ea61b729d317ae6a0aef16
Mohammad Abdolirad <m.abdolirad@gmail.com>
2023-01-12 09:47:32 +0100

    Use fzf instead of mcfly

---
+++ home-manager/fzf/default.nix
@@ -0,0 +1,24 @@
+{ inputs, lib, config, pkgs, ... }:
+
+{
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    defaultOptions = [];
+    colors = {
+      fg = "#f8f8f2";
+      bg = "#282a36";
+      hl = "#bd93f9";
+      "fg+" = "#f8f8f2";
+      "bg+" = "#44475a";
+      "hl+" = "#bd93f9";
+      info = "#ffb86c";
+      prompt = "#50fa7b";
+      pointer = "#ff79c6";
+      marker = "#ff79c6";
+      spinner = "#ffb86c";
+      header = "#6272a4";
+    };
+  };
+}
