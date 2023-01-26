76b1f9905c5f0b91d3bd6e8f8704612096f0e5c6
Mohammad Abdolirad <m.abdolirad@gmail.com>
2023-01-26 21:29:22 +0100

    Install protobuf and its dependencies

--- home-manager/go/default.nix
+++ home-manager/go/default.nix
@@ -1,6 +1,12 @@
 { inputs, lib, config, pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    protobuf
+    protoc-gen-go
+    protoc-gen-go-grpc
+  ];
+
   programs.go = {
     enable = true;
     goPath = "go"; # Primary GOPATH relative to HOME.
