1f8e1980a0abd94bea5060aae120747b7af6da33
Mohammad Abdolirad <m.abdolirad@gmail.com>
2024-08-22 10:46:35 +0200

    Enable custom font and Dracula theme for console (TTY)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -72,7 +72,8 @@
 
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelModules = ["kvm-amd" "kvm-intel"];
+    kernelParams = [ "i915.fastboot=1" ];
+    kernelModules = ["i915" "kvm-amd" "kvm-intel"];
     supportedFilesystems = ["ntfs"];
     loader = {
       efi.canTouchEfiVariables = true;
@@ -104,12 +105,29 @@
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     earlySetup = true;
-    font = "Lat2-Terminus16";
+    font = "ter-124b";
     keyMap = "us";
-    #packages = with pkgs; [
-    #  terminus_font
-    #];
-    #useXkbConfig = true; # use xkbOptions in tty.
+    packages = with pkgs; [
+      terminus_font
+    ];
+    colors = [
+      "282a36"    # redefine 'black'          as 'dracula-bg'
+      "6272a4"    # redefine 'bright-black'   as 'dracula-comment'
+      "ff5555"    # redefine 'red'            as 'dracula-red'
+      "ff7777"    # redefine 'bright-red'     as '#ff7777'
+      "50fa7b"    # redefine 'green'          as 'dracula-green'
+      "70fa9b"    # redefine 'bright-green'   as '#70fa9b'
+      "f1fa8c"    # redefine 'brown'          as 'dracula-yellow'
+      "ffb86c"    # redefine 'bright-brown'   as 'dracula-orange'
+      "bd93f9"    # redefine 'blue'           as 'dracula-purple'
+      "cfa9ff"    # redefine 'bright-blue'    as '#cfa9ff'
+      "ff79c6"    # redefine 'magenta'        as 'dracula-pink'
+      "ff88e8"    # redefine 'bright-magenta' as '#ff88e8'
+      "8be9fd"    # redefine 'cyan'           as 'dracula-cyan'
+      "97e2ff"    # redefine 'bright-cyan'    as '#97e2ff'
+      "f8f8f2"    # redefine 'white'          as 'dracula-fg'
+      "ffffff"    # redefine 'bright-white'   as '#ffffff'
+    ];
   };
 
   # X11 windowing system.
