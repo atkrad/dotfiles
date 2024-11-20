e3b16129527e0e26d4aac9a58f1c6f3259fa1201
Mohammad Abdolirad <m.abdolirad@gmail.com>
2024-11-20 10:48:10 +0100

    Refactor starship settings

--- home-manager/modules/starship/default.nix
+++ home-manager/modules/starship/default.nix
@@ -10,20 +10,95 @@
     enableBashIntegration = true;
     enableZshIntegration = true;
     settings = {
-      aws.style = "bold #ffb86c";
-      cmd_duration.style = "bold #f1fa8c";
-      directory.style = "bold #50fa7b";
-      hostname.style = "bold #ff5555";
-      git_branch.style = "bold #ff79c6";
-      git_status.style = "bold #ff5555";
+      php.symbol = " ";
+      aws = {
+        style = "bold orange";
+        symbol = "  ";
+      };
+      buf.symbol = " ";
+      c.symbol = " ";
+      conda.symbol = " ";
+      crystal.symbol = " ";
+      cmd_duration.style = "bold yellow";
+      dart.symbol = " ";
+      directory = {
+        read_only = " 󰌾";
+        style = "bold green";
+      };
+      docker_context.symbol = " ";
+      elixir.symbol = " ";
+      elm.symbol = " ";
+      fennel.symbol = " ";
+      fossil_branch.symbol = " ";
+
+      git_branch = {
+        symbol = " ";
+        style = "bold pink";
+      };
+
+      git_commit = {
+        tag_symbol = "  ";
+        tag_disabled = false;
+      };
+
+      golang.symbol = " ";
+      guix_shell.symbol = " ";
+      haskell.symbol = " ";
+      haxe.symbol = " ";
+      hg_branch.symbol = " ";
+
+      hostname = {
+        ssh_symbol = " ";
+        style = "bold purple";
+      };
+      java.symbol = " ";
+      julia.symbol = " ";
+      kotlin.symbol = " ";
+      lua.symbol = " ";
+      memory_usage.symbol = "󰍛 ";
+      meson.symbol = "󰔷 ";
+      nim.symbol = "󰆥 ";
+      nix_shell.symbol = " ";
+      nodejs.symbol = " ";
+      ocaml.symbol = " ";
+
+      os.symbols = {
+        Linux = " ";
+        NixOS = " ";
+        Raspbian = " ";
+        Unknown = " ";
+      };
+      git_status.style = "bold red";
       helm.symbol = "☸️  ";
       username = {
         format = "[$user]($style) on ";
-        style_user = "bold #bd93f9";
+        style_user = "bold cyan";
       };
       character = {
-        success_symbol = "[λ](bold #f8f8f2)";
-        error_symbol = "[λ](bold #ff5555)";
+        success_symbol = "[λ](bold green)";
+        error_symbol = "[λ](bold red)";
+      };
+      sudo = {
+        disabled = false;
+        style = "bold green";
+      };
+
+      # Use the color palette
+      palette = "dracula";
+
+      # Define Dracula color palette
+      palettes.dracula = {
+        background = "#282a36";
+        current_line = "#44475a";
+        foreground = "#f8f8f2";
+        comment = "#6272a4";
+        cyan = "#8be9fd";
+        green = "#50fa7b";
+        orange = "#ffb86c";
+        pink = "#ff79c6";
+        purple = "#bd93f9";
+        red = "#ff5555";
+        yellow = "#f1fa8c";
       };
     };
   };
